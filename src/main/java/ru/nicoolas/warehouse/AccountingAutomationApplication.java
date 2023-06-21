903619d249313a801fe604367cc3b4804a698bfd
NikolayL <nicoolas.ru@gmail.com>
2023-06-21 11:02:01 +0300

    First commit

---
+++ src/main/java/ru/nicoolas/warehouse/AccountingAutomationApplication.java
@@ -0,0 +1,13 @@
+package ru.nicoolas.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nicoolas/warehouse/AccountingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nicoolas.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
